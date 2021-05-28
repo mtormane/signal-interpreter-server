4f383576658fc1588b2ec5903fc4c82a4e3617cb
Törmänen, Mikael <mikael.tormanen@volvocars.com>
2021-05-28 16:21:36 +0200

    Assignment 1 finished

---
+++ signal_interpreter_server/main.py
@@ -0,0 +1,8 @@
+from signal_interpreter_server.routes import signal_interpreter_app
+
+def main():
+    signal_interpreter_app.run()
+
+if __name__ == "__main__":
+    main()
+
---
+++ signal_interpreter_server/routes.py
@@ -0,0 +1,12 @@
+# routes.py
+from flask import Flask, request
+
+signal_interpreter_app = Flask(__name__)
+
+@signal_interpreter_app.route("/", methods=["POST"])
+def mirror_data():
+    data = request.get_json()
+    return data
+
+
+#signal_interpreter_app.run()
